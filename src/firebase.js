5cb39cc2c3106da9b4cd182d605e93a5dc72e1eb
AhmadJamshaid <ahmadjamshaid2003@gmail.com>
2026-01-07 01:29:39 +0500

    Sync environment names

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyBJ2q-5IKqomRzIF0X4LmVYhJJyYSXlABM",
+  authDomain: "nust-market.firebaseapp.com",
+  projectId: "nust-market",
+  storageBucket: "nust-market.appspot.com",
+  messagingSenderId: "353984015464",
+  appId: "1:353984015464:web:6d77f25ffe62d674339fdd"
 };
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export default app;
